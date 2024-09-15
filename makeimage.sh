186e6da4b4c76a57355c759d23fd828b4c556539
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-15 20:29:41 +0800

    - update makeimage.sh

--- makeimage.sh
+++ makeimage.sh
@@ -19,16 +19,18 @@ cd /opt; \
 export PATH=/opt/gcc-arm-10.3-2021.07-aarch64-arm-none-linux-gnueabihf/bin:$PATH
 
 cd /workspace/u-boot-imx6
-export CROSS_COMPILE="arm-none-linux-gnueabihf-"
+# export CROSS_COMPILE="arm-none-linux-gnueabihf-"
+export CROSS_COMPILE="arm-linux-gnueabihf-"
 export UBOOT_CONFIG="mx6cuboxi_defconfig"
 
 gcc --version
 
 uname -m
-file arm-none-linux-gnueabihf-gcc
+file arm-linux-gnueabihf-gcc
 
 # which cc
 announce "Building u-boot"
-make CC=arm-none-linux-gnueabihf-gcc "$UBOOT_CONFIG"
+# make CC=arm-none-linux-gnueabihf-gcc "$UBOOT_CONFIG"
+make CC=arm-linux-gnueabihf-gcc "$UBOOT_CONFIG"
 make 
 announce "image build appears to have been successful"
