a04423d02c375e8991bdb282673cfb8ca44d942b
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-15 19:55:33 +0800

    - update makeimage.sh

--- makeimage.sh
+++ makeimage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo 'User: $(whoami) UID: $(id -u) GID: $(id -g)'
+
 # an echo that will stand out in the logs
 function announce () {
     echo "##########################################################################################"
@@ -14,7 +16,7 @@ export PATH=/opt/gcc-arm-10.3-2021.07-aarch64-arm-none-linux-gnueabihf/bin:$PATH
 cd /workspace/u-boot-imx6
 export CROSS_COMPILE="arm-none-linux-gnueabihf-"
 export UBOOT_CONFIG="mx6cuboxi_defconfig"
-which cc
+# which cc
 announce "Building u-boot"
 make CC=arm-none-linux-gnueabihf-gcc "$UBOOT_CONFIG"
 make 
