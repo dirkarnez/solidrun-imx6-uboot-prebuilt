86ff21803cd7b3d88222c1c114034bcd8c78e487
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-24 22:57:10 +0800

    - update makeimage.sh

--- makeimage.sh
+++ makeimage.sh
@@ -11,21 +11,26 @@ function announce () {
 
 set -e
 
-export PATH=/opt/gcc-arm-10.3-2021.07-aarch64-arm-none-linux-gnueabihf/bin:$PATH
-#  CC=arm-none-linux-gnueabihf-gcc
-cd /workspace/u-boot-imx6
-export CROSS_COMPILE="arm-none-linux-gnueabihf-"
 # export UBOOT_CONFIG="mx6cuboxi_defconfig"
-which cc
-announce "Building u-boot"
 # make mrproper
-make mx6cuboxi_defconfig
-echo "CONFIG_SPL_BOOT_DEVICE_SDHC=y" >> .config # boot from sd card
-make -j8
-announce "image build appears to have been successful"
-announce "copying files"
-install -v -m644 -D ./SPL /dist/SPL
-install -v -m644 -D ./u-boot.img /dist/u-boot.img
+
+export PATH="/opt/gcc-linaro-7.5.0-2019.12-i686_arm-linux-gnueabihf/bin:$PATH" && \
+arm-linux-gnueabihf-gcc --version && \
+cd /workspace/u-boot-imx6 && \
+export CROSS_COMPILE="arm-linux-gnueabihf-" && \
+announce "Building u-boot" && \
+make mx6cuboxi_defconfig && \
+echo "CONFIG_SPL_BOOT_DEVICE_SDHC=y" >> .config && \
+make -j8 && \
+announce "image build appears to have been successful" && \
+announce "copying files" && \
+install -v -m644 -D ./SPL /dist/SPL && \
+install -v -m644 -D ./u-boot.img /dist/u-boot.img && \
 announce "files copied"
 
+
+
+# for file in $(find source -type f -name *.py); do
+#     install -m 644 -D ${file} dest/${file#source/}
+# done
 
